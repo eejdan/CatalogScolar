2fd7ff90c3ac35abd6f5e3ecd9e23fb418de4010
BuildTools <unconfigured@null.spigotmc.org>
2021-02-15 10:21:05 +0200

    more changes, not yet working build

---
+++ v1/dbpopulation.js
@@ -0,0 +1,27 @@
+
+require('dotenv').config();
+
+const mysql = require('mysql');
+
+const hash = require('hash.js');
+
+
+var con = mysql.createConnection({
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASS,
+    database: process.env.DBNAME
+})
+setTimeout(function(){
+con.connect((err) => {
+    if(err) throw err;
+    passmain = hash.sha256().update("passmain").digest('hex');
+    passp1 = hash.sha256().update("passp1").digest('hex');
+    passp2 = hash.sha256().update("passp2").digest('hex');
+    sql = `INSERT INTO \`user\` (\`numar_matricol\`,\`password\`, \`p1_password\`, \`p2_password\`) VALUES ('0', '${passmain}', '${passp1}', '${passp2}')`;
+    con.query(sql, (err, result, fields) => {
+        if(err) throw err;
+    })
+    console.log("da");
+})
+}, 5000);
--- v1/index.js
+++ v1/index.js
@@ -11,7 +11,8 @@ const { render } = require("ejs");
 var mysql = require('mysql')
 
 var app = express();
-var con = mysql.createConnection({
+var sqlPool = mysql.createPool({
+    connectionLimit : 10,
     host: process.env.DBHOST,
     user: process.env.DBUSER,
     password: process.env.DBPASS,
@@ -27,65 +28,86 @@ app.use(cookieParser(process.env.COOKIESECRET))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // do the login and db stuff
-g_renderParams = { 
-    "parts": [
-        {"name": "header","dir":"enclosure/part/header","styleDir": "css/header.css"},
-        {"name": "footer","dir":"enclosure/part/footer","styleDir": "css/footer.css"}//
-    ]
-}
 //TBD keep queries as string variables
-renderLogin = () => {
+
+// IMPORTANT: failsafe instead of throw
+//make logout delete sid
+// will use sid token for session id
+app.get('/', (req,res) => {
     let renderParams = {
         "parts": [
-            {"name": "logbox", "dir":"enclosure/part/logbox", "styleDir": "css/logbox.css"}
+            {"name": "logbox", "dir":"enclosure/part/logbox", "styleDir": "logbox.css"}
+        ]
+    }
+    if(!req.signedCookies) {
+        res.render('index', renderParams);
+        return;
+    }
+    sqlPool.getConnection((err, con) => {
+        if(err) throw err;
+        var user;
+        con.query(`SELECT * FROM session WHERE sid='${req.signedCookies.sid}'`, (err, result, fields) => {
+            if(err) throw err;
+            if(!result.length) {
+                res.render('index', renderParams);
+                return;
+            }
+            console.log(new Date(result[0].last))
+        })
+    })
+    console.log(new Date('1-01-1999'));
+    res.render('index', renderParams);
+})
+//req.body.password //req.body.matricol
+app.post('/login', (req,res) => {
+    getUserSql = `SELECT * FROM \`user\` WHERE numar_matricol=\`${req.body.matricol}\``;
+    var correctPass = false;
+    con.connect((err) => {
+        if(err) throw err;
+        con.query(getUserSql,  (err, result, fields) => {
+            if(err) throw err;
+            if(result.length > 0) {
+                if(result[0]) {
+                    hash = shaenc(req.body.password);
+                    switch(hash) {
+                        case result[0].password:
+                            correctPass = true; break;
+                        case result[0].p1_password:
+                            correctPass = true; break;
+                        case result[0].p2_password:
+                            correctPass = true; break;
+                    }
+                }
+            }
+        })
+    })
+    if(!correctPass) {
+        renderLogin
+        return;
+    }
+    var alpha = 'abcdefg'
+    var cookie = '';
+    for(var i = 0;i<16;i++) {
+        cookie += alpha[Math.floor(Math.random() * 7)];
+    }
+    cookie = shaenc(cookie);
+    con.connect((err) => {
+        if(err) throw err;
+        makeSessionSql = `INSERT INTO \`session\` (\`numar_matricol\`, \`sid\`) VALUES ('${req.body.matricol}', '${cookie}', 'current_timestamp()')`;
+        con.query(`DELETE * FROM session WHERE sid=\`${req.signedCookies.sid}\``, (err, result, fields) => {
+            if(err) throw err;    
+        })
+    })
+    res.cookie('sid', cookie, { signed: true });
+    let renderParams = {
+        "parts": [
+            {"name": "elev", "dir":"enclosure/part/elev", "styleDir": "elev.css"}
         ]
     }
     let final_renderParams = {
         "parts": g_renderParams.parts.concat(renderParams.parts)
     }
-    res.render('index', final_renderParams)
-}
-//make logout delete sid
-// will use sid token for session id
-app.get('/', (req,res) => {
-    if(!req.signedCookies) {
-        renderLogin
-        return;
-    }
-    con.connect((err) => {
-        if(err) throw err;
-        con.query(`SELECT * FROM session WHERE sid=\`${req.signedCookies.sid}\``, (err, result, fields) => {
-            if(err) throw err;
-            if(!result[0].numar_matricol) {
-                renderLogin
-                return;
-            }
-            if(result[0].last) {
-                con.query(`UPDATE session SET last = CURRENT_TIMESTAMP WHERE sid=\`${req.signedCookies.sid}\``, (err, result, fields) => {
-                    if(err) throw err;
-                })
-            } else {
-                renderLogin
-                return;
-            }
-        })
-    })
-    var renderParams = {
-        "parts": [
-            
-        ]
-    } 
-    var final_renderParams = {
-        "parts": g_renderParams.parts.concat(renderParams.parts)
-    }
-    res.render('index', final_renderParams)
-})
-//req.body.password //req.body.username
-app.post('/login', (req,res) => {
-    //build db user
-    //check if returned empty set
-    hashedPassword = hash.sha256().update(req.body.password).digest('hex');
-    
+    res.render('index', final_renderParams);
 
 })
 app.post('/logout', (req,res) => {
@@ -93,6 +115,7 @@ app.post('/logout', (req,res) => {
         if(err) throw err;
         con.query(`DELETE * FROM session WHERE sid=\`${req.signedCookies.sid}\``, (err, result, fields) => {
             if(err) throw err;    
+            
         })
     })
     renderLogin
@@ -128,4 +151,7 @@ function logFailsafe(type) {
         "type": type,
         "context": context
     };
+}
+function shaenc(str) {
+    return hash.sha256().update(str).digest('hex');
 }
