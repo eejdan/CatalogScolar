bb78469459451d556c1585e823b427787e3e9e55
BuildTools <unconfigured@null.spigotmc.org>
2021-02-17 17:06:40 +0200

    push pentru version control

--- v1/index.js
+++ v1/index.js
@@ -2,6 +2,8 @@
 console.log('Daniel Birleanu 2021 Copyright. All rights reserved.');
 require('dotenv').config();
 
+init();
+
 const express = require("express");
 const hash = require('hash.js')
 const cookieParser = require("cookie-parser");
@@ -28,12 +30,7 @@ app.use(cookieParser(process.env.COOKIESECRET))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-// do the login and db stuff
-//TBD keep queries as string variables
 
-// IMPORTANT: failsafe instead of throw
-//make logout delete sid
-// will use sid token for session id
 app.get('/', async (req,res) => {
     var returned = false;
     let renderParams = {
@@ -45,17 +42,13 @@ app.get('/', async (req,res) => {
         returned = true;
         return res.render('index', renderParams);
     }
-    var valid=false;
-    console.log('test1');
     sqlPool.getConnection((err, con) => {
         if(err) { return res.render('index', renderParams);  }
         validateSidSql = `SELECT * FROM session WHERE sid='${req.signedCookies.sid}'`;
-        console.log('test1.3');
         con.query(validateSidSql, (err, result, fields) => {
             if(err) { returned = true; return res.render('index', renderParams);  }
             if(!result.length) {
                 res.clearCookie('sid');
-                console.log('test1.5');
                 returned = true;
                 return res.render('index', renderParams);
             }
@@ -64,7 +57,6 @@ app.get('/', async (req,res) => {
                     {"name": "elev", "dir":"enclosure/part/elev", "styleDir": "elev.css"}
                 ]
             }
-            console.log('test1.7');
             let date = new Date(result[0].last);
             return res.render('index', renderParams);
         })
@@ -76,11 +68,11 @@ app.post('/login', (req,res) => {
     console.log(req.body.password);
     var returned = false;
     sqlPool.getConnection((err, con) => {
-        if(err) throw console.log(err);
+        if(err) return console.log(err);
         var correctPass = false;
         getUserSql = `SELECT * FROM \`user\` WHERE numar_matricol='${req.body.matricol}'`;
         con.query(getUserSql,  (err, result, fields) => {
-            if(err) throw err;
+            if(err) return console.log(err);
             if(result.length > 0) {
                 if(result[0]) {
                     let phash = shaenc(req.body.password);
@@ -121,48 +113,64 @@ app.post('/logout', (req,res) => {
     res.clearCookie('sid');
     console.log('user logout '+ req.signedCookies.sid); 
     sqlPool.getConnection((err, con) => {
-        if(err) throw console.log(err);
+        if(err) return console.log(err);
         con.query(`DELETE FROM session WHERE sid='${req.signedCookies.sid}'`, (err, result, fields) => {
-            if(err) console.log(err);    
+            if(err) return console.log(err);    
             res.redirect('/');
         })
     })
 })
 //FIX THIS
-app.get('/elev/', (req,res) => {
+app.post('/elev/', (req,res) => {
     //validate cookie
     if(!req.signedCookies.sid) {
-        returned = true;
-        return res.render('index', renderParams);
+        return res.send(401);
     }
-    renderParams = {
-        "parts": [
-            {"name": "elev", "dir":"enclosure/part/elev", "styleDir": "elev.css"}
-        ]
-    }
-    sqlPool.getConnection((err, con) => { 
-
+    let build = {}
+    sqlPool.getConnection((err, con) => {
+        if(err) return console.log(err);
+        let validateSidSql = `SELECT * FROM session WHERE sid='${req.signedCookies.sid}'`;
+        con.query(validateSidSql, (err, result, fields) => {
+            if(err) { console.log(err); return res.send(500)  }
+            if(!result.length) {
+                res.clearCookie(404)
+                return res.send(404);
+            }
+            refreshSid(req.signedCookies.sid);
+            let build = {
+                "nota": [],
+                "abs": []
+            }
+            let getElevClasaSql = `SELECT * FROM \`clasa\` WHERE `
+            
+            con.query()
+                let getElevNotaSql = `SELECT * FROM \`nota\` WHERE numar_matricol=${result[0].numar_matricol}`;
+                con.query(getElevNotaSql, (err, result, field) => {
+                    if(err) { console.log(err); res.send(500); }
+                    result.forEach((sres)=> {
+                    build.nota[build.nota.length] = {"materie":sres.disciplina,"nota":sres.nota,"data":sres.data} 
+                    });
+                    let getAbsentaSql = `SELECT * FROM \`absenta\` WHERE numar_matricol=${result[0].numar_matricol}`
+                    con.query(getAbsentaSql, (err, result, field) => {
+                        if(err) { console.log(err); res.send(500) }
+                        if(!result.length) {
+                            return res.send(build);
+                        }
+                        result.forEach((sres) => {
+                            build.abs[build.abs.length] = {}
+                        })
+                    })
+                });
+        })
     })
 }) 
 
 app.listen(3030);
 
-//make signer
-
-//make init
-
-/*{ 
-    //delete sessions
-    let unclosedSessions
-    console.log("Keeping failed")
+async function init() {
+    cleanSids()
 }
-function logFailsafe() {
-
-}
-*/
 logErrVault = [];
-//keep in memory and retry later
-//
 function logFailsafe(type) {
     logErrVault[logErrVault.length] = { 
         "type": type,
@@ -172,3 +180,43 @@ function logFailsafe(type) {
 function shaenc(str) {
     return hash.sha256().update(str).digest('hex');
 }
+async function refreshSid(lsid) {
+    sqlPool.getConnection((err, con) => {
+        if(err) return console.log(err);
+        let updateSidSql = `UPDATE \`session\` SET data='${buildDbDate()}' WHERE sid='${lsid}'`;
+        con.query(updateSidSql, (err, result, fields) => { 
+            if(err) return console.log(err);
+        })
+    })
+}
+async function cleanSids() {
+    sqlPool.getConnection((err, con) => {
+        if(err) return console.log(err);
+        let updateSidSql = `DELETE FROM \`session\` WHERE data < ${buildDbDate}`;
+        con.query(updateSidSql, (err, result, fields) => { 
+            if(err) return console.log(err);
+        })
+    })
+    setTimeout(cleanSids, 43200000)
+}
+
+function buildDbDate(a) {
+    let basicDate = new Date();
+    let mth = basicDate.getMonth()+1;
+    if(mth<10) {
+        mth = '0'+mth; 
+    }
+    let strDate = `${basicDate.getFullYear()}-${mth}-${basicDate.getDate()}`;
+    return strDate;
+}
+
+//TODO: elev.ejs: remove onclick, add jquery event handlers instead
+
+
+/* Notes:
+Dates are managed by the web server.
+
+
+
+
+*/
