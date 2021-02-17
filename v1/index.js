776eb8fa374881e31d01d49503581bdbbaacdcd8
BuildTools <unconfigured@null.spigotmc.org>
2021-02-17 10:42:27 +0200

    logout login done

--- v1/index.js
+++ v1/index.js
@@ -12,6 +12,7 @@ var mysql = require('mysql')
 
 var app = express();
 var sqlPool = mysql.createPool({
+    waitForConnections: true,
     connectionLimit : 10,
     host: process.env.DBHOST,
     user: process.env.DBUSER,
@@ -33,43 +34,57 @@ app.use(bodyParser.json())
 // IMPORTANT: failsafe instead of throw
 //make logout delete sid
 // will use sid token for session id
-app.get('/', (req,res) => {
+app.get('/', async (req,res) => {
+    var returned = false;
     let renderParams = {
         "parts": [
             {"name": "logbox", "dir":"enclosure/part/logbox", "styleDir": "logbox.css"}
         ]
     }
-    if(!req.signedCookies) {
-        res.render('index', renderParams);
-        return;
+    if(!req.signedCookies.sid) {
+        returned = true;
+        return res.render('index', renderParams);
     }
+    var valid=false;
+    console.log('test1');
     sqlPool.getConnection((err, con) => {
-        if(err) throw err;
-        var user;
-        con.query(`SELECT * FROM session WHERE sid='${req.signedCookies.sid}'`, (err, result, fields) => {
-            if(err) throw err;
+        if(err) { return res.render('index', renderParams);  }
+        validateSidSql = `SELECT * FROM session WHERE sid='${req.signedCookies.sid}'`;
+        console.log('test1.3');
+        con.query(validateSidSql, (err, result, fields) => {
+            if(err) { returned = true; return res.render('index', renderParams);  }
             if(!result.length) {
-                res.render('index', renderParams);
-                return;
+                res.clearCookie('sid');
+                console.log('test1.5');
+                returned = true;
+                return res.render('index', renderParams);
             }
-            console.log(new Date(result[0].last))
+            renderParams = {
+                "parts": [
+                    {"name": "elev", "dir":"enclosure/part/elev", "styleDir": "elev.css"}
+                ]
+            }
+            console.log('test1.7');
+            let date = new Date(result[0].last);
+            return res.render('index', renderParams);
         })
+        if(returned) { return; }
     })
-    console.log(new Date('1-01-1999'));
-    res.render('index', renderParams);
 })
 //req.body.password //req.body.matricol
 app.post('/login', (req,res) => {
-    getUserSql = `SELECT * FROM \`user\` WHERE numar_matricol=\`${req.body.matricol}\``;
-    var correctPass = false;
-    con.connect((err) => {
-        if(err) throw err;
+    console.log(req.body.password);
+    var returned = false;
+    sqlPool.getConnection((err, con) => {
+        if(err) throw console.log(err);
+        var correctPass = false;
+        getUserSql = `SELECT * FROM \`user\` WHERE numar_matricol='${req.body.matricol}'`;
         con.query(getUserSql,  (err, result, fields) => {
             if(err) throw err;
             if(result.length > 0) {
                 if(result[0]) {
-                    hash = shaenc(req.body.password);
-                    switch(hash) {
+                    let phash = shaenc(req.body.password);
+                    switch(phash) {
                         case result[0].password:
                             correctPass = true; break;
                         case result[0].p1_password:
@@ -78,61 +93,65 @@ app.post('/login', (req,res) => {
                             correctPass = true; break;
                     }
                 }
+            } 
+            if(!correctPass) {
+                returned = true;
+                return res.redirect('/');
             }
+            let cookie = '';
+            {
+                let alpha = 'abcdefg';
+                for(let i = 0;i<16;i++) {
+                    cookie += alpha[Math.floor(Math.random() * 7)];
+                }
+                cookie = shaenc(cookie);
+            }
+            let makeSessionSql = `INSERT INTO \`session\` (\`numar_matricol\`, \`sid\`) VALUES ('${req.body.matricol}', '${cookie}')`;
+            con.query(makeSessionSql, async (err, result, fields) => {
+                if(err) throw err;    
+                await res.cookie('sid', cookie, { signed: true });
+                return res.redirect('/');
+            })
+        })
+        if(returned) {return;}
+    })
+})
+//logout done
+app.post('/logout', (req,res) => {
+    res.clearCookie('sid');
+    console.log('user logout '+ req.signedCookies.sid); 
+    sqlPool.getConnection((err, con) => {
+        if(err) throw console.log(err);
+        con.query(`DELETE FROM session WHERE sid='${req.signedCookies.sid}'`, (err, result, fields) => {
+            if(err) console.log(err);    
+            res.redirect('/');
         })
     })
-    if(!correctPass) {
-        renderLogin
-        return;
+})
+//FIX THIS
+app.get('/elev/', (req,res) => {
+    //validate cookie
+    if(!req.signedCookies.sid) {
+        returned = true;
+        return res.render('index', renderParams);
     }
-    var alpha = 'abcdefg'
-    var cookie = '';
-    for(var i = 0;i<16;i++) {
-        cookie += alpha[Math.floor(Math.random() * 7)];
-    }
-    cookie = shaenc(cookie);
-    con.connect((err) => {
-        if(err) throw err;
-        makeSessionSql = `INSERT INTO \`session\` (\`numar_matricol\`, \`sid\`) VALUES ('${req.body.matricol}', '${cookie}', 'current_timestamp()')`;
-        con.query(`DELETE * FROM session WHERE sid=\`${req.signedCookies.sid}\``, (err, result, fields) => {
-            if(err) throw err;    
-        })
-    })
-    res.cookie('sid', cookie, { signed: true });
-    let renderParams = {
+    renderParams = {
         "parts": [
             {"name": "elev", "dir":"enclosure/part/elev", "styleDir": "elev.css"}
         ]
     }
-    let final_renderParams = {
-        "parts": g_renderParams.parts.concat(renderParams.parts)
-    }
-    res.render('index', final_renderParams);
+    sqlPool.getConnection((err, con) => { 
 
-})
-app.post('/logout', (req,res) => {
-    con.connect((err) => {
-        if(err) throw err;
-        con.query(`DELETE * FROM session WHERE sid=\`${req.signedCookies.sid}\``, (err, result, fields) => {
-            if(err) throw err;    
-            
-        })
     })
-    renderLogin
-})
-//FIX THIS
-app.get('/elev/', (req,res) => {
-    //validate sid 
-    //TBD build up data
-    udata = {}
-    res.json(udata)
 }) 
+
+app.listen(3030);
 
 //make signer
 
 //make init
 
-{ 
+/*{ 
     //delete sessions
     let unclosedSessions
     console.log("Keeping failed")
@@ -140,9 +159,7 @@ app.get('/elev/', (req,res) => {
 function logFailsafe() {
 
 }
-
-
-app.listen(3030);
+*/
 logErrVault = [];
 //keep in memory and retry later
 //
